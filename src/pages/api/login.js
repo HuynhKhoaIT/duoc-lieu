be1b2fe22cde9526c0f4a9a6260ecc07276343a1
Nguyễn Huỳnh Khoa <114349590+HuynhKhoaIT@users.noreply.github.com>
2025-09-01 21:40:32 +0700

    fix login

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -2,35 +2,42 @@
 import { serialize } from "cookie";
 
 import apiConfig from "@/constants/apiConfig";
-
 export default async function handler(req, res) {
-    const { phone_number, password } = req.body;
+    if (req.method !== "POST") {
+        res.setHeader("Allow", [ "POST" ]);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
 
-    // Gọi API backend thực sự để check login
-    const result = await fetch(apiConfig.account.loginBasic.url, {
-        method: "POST",
-        body: JSON.stringify({ phone_number, password }),
-        headers: { "Content-Type": "application/json" },
-    }).then((r) => r.json());
+    try {
+        const { phone_number, password } = req.body;
+        const result = await fetch(apiConfig.account.loginBasic.url, {
+            method: "POST",
+            body: JSON.stringify({ phone_number, password }),
+            headers: { "Content-Type": "application/json" },
+        }).then((r) => r.json());
 
-    console.log("result", result);
-    if (result.token) {
-        res.setHeader(
-            "Set-Cookie",
-            serialize("token", result.token, {
-                path: "/",
-                httpOnly: true,
-                secure: process.env.NODE_ENV === "production",
-                sameSite: "strict",
-                maxAge: 60 * 60 * 24 * 365, // 365 ngày
-            }),
-        );
-        res.status(200).json({
-            success: true,
-            token: result.token,
-            user: result.user,
-        });
-    } else {
-        res.status(401).json({ success: false });
+        if (result.token) {
+            res.setHeader(
+                "Set-Cookie",
+                serialize("token", result.token, {
+                    path: "/",
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: "strict",
+                    maxAge: 60 * 60 * 24 * 365,
+                }),
+            );
+            return res
+                .status(200)
+                .json({
+                    success: true,
+                    token: result.token,
+                    user: result.user,
+                });
+        } else {
+            return res.status(401).json({ success: false });
+        }
+    } catch (err) {
+        return res.status(500).json({ success: false, message: err.message });
     }
 }
