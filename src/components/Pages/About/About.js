bc8d59d6130daf9b1c0576af7fe03fd26198a71a
Nguyễn Huỳnh Khoa <114349590+HuynhKhoaIT@users.noreply.github.com>
2025-09-07 00:35:07 +0700

    about page

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -5,13 +5,13 @@ import FeatureSection from "./FeatureSection/FeatureSection";
 import LatestNews from "./LatestNews/LatestNews";
 import ShopBanner from "./ShopBanner/ShopBanner";
 
-export default function AboutPage() {
+export default function AboutPage({newsListData}) {
     return (
         <>
             <Breadcrumb title={"Giới thiệu"} />
             <FeatureSection />
             <ShopBanner />
-            <LatestNews />
+            <LatestNews newsListData={newsListData}/>
             <LogoCarousel />
         </>
     );
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,13 +1,35 @@
 import Layout from "@/components/layouts/Layout";
 import AboutPage from "@/components/Pages/About/About";
+import apiConfig from "@/constants/apiConfig";
 
-function About() {
-
-    return (
-        <AboutPage/>
-    );
+function About({ newsList }) {
+    return <AboutPage newsListData={newsList} />;
 }
+export async function getStaticProps() {
+    try {
+        const resList = await fetch(apiConfig.news.getList.url, {
+            cache: "force-cache",
+        });
 
+        const newsList = resList.ok ? await resList.json() : [];
+
+        return {
+            props: {
+                newsList,
+                errorList: resList.ok ? null : `Error ${resList.status}`,
+            },
+            revalidate: 300, // sau 300s sẽ build lại
+        };
+    } catch (err) {
+        return {
+            props: {
+                newsList: [],
+                errorList: err.message,
+            },
+            revalidate: 60, // fallback nhanh hơn nếu lỗi
+        };
+    }
+}
 About.getLayout = function getLayout(page) {
     return <Layout>{page}</Layout>;
 };
