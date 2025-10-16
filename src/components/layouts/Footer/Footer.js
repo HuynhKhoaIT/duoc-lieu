7a28c26fd645c71d795b2b8fec3dc9388b8a693c
Nguyễn Huỳnh Khoa <114349590+HuynhKhoaIT@users.noreply.github.com>
2025-10-16 10:14:59 +0700

    footer

--- src/components/layouts/Footer/Footer.js
+++ src/components/layouts/Footer/Footer.js
@@ -10,20 +10,29 @@ export default function Footer() {
             <div className={styles.footerArea}>
                 <div className="container mx-auto">
                     <div className="flex flex-wrap -mx-4">
-                        <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+                        <div className="w-full md:w-1/2 lg:w-1/2 px-3">
                             <div
                                 className={`${styles.footerBox} ${styles.aboutWidget}`}
                             >
                                 <h2 className={styles.widgetTitle}>
-                                    Về chúng tôi
+                                    CTY TNHH MTV TÂM PHÚC THÀNH
                                 </h2>
-                                <p>
-                                    Một công trình nghiên cứu kinh nghiệm 22 năm
-                                    của Dược Sĩ Kim Ngọc, chuyên tâm, chuyên
-                                    sâu, lĩnh vực chăm sóc tóc bằng tinh dược
-                                    liệu. Mang lại giá trị thiết thực, Tin Cậy,
-                                    An Toàn, Hiệu Quả cho người tiêu dùng.
-                                </p>
+                                <p>Người đại diện: Đặng Thị Kim Ngọc</p>
+                                <ul className="!list-none space-y-2 !pl-0">
+                                    <li>
+                                        <i className="fas fa-map-marker-alt mr-2"></i>
+                                        215 ấp 10, xã Vị Thuỷ, TP Cần Thơ
+                                    </li>
+                                    <li>
+                                        <i className="fas fa-envelope mr-2"></i>
+                                        daugoiduoclieun22@gmail.com
+                                    </li>
+                                    <li>
+                                        <i className="fas fa-phone-alt mr-2"></i>
+                                        0941.003.004
+                                    </li>
+                                    <li>MST: 6300288132</li>
+                                </ul>
                                 {/* <div
                                     style={{
                                         position: "relative",
@@ -42,31 +51,31 @@ export default function Footer() {
                         </div>
 
                         {/* Contact */}
-                        <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+                        {/* <div className="w-full md:w-1/2 lg:w-1/4 px-3">
                             <div
                                 className={`${styles.footerBox} ${styles.getInTouch}`}
                             >
                                 <h2 className={styles.widgetTitle}>Liên hệ</h2>
                                 <ul>
                                     <li>
-                                        Địa chỉ: Âp 10, xã Vị Thuỷ, thành phố
-                                        Cần Thơ
+                                        Địa chỉ: 215 ấp 10, xã Vị Thuỷ, TP Cần
+                                        Thơ
                                     </li>
                                     <li>Hotline: 0941.003.004</li>
                                 </ul>
                             </div>
-                        </div>
+                        </div> */}
 
                         {/* Pages */}
-                        <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+                        <div className="w-full md:w-1/2 lg:w-1/4 px-3">
                             <div
                                 className={`${styles.footerBox} ${styles.pages}`}
                             >
                                 <h2 className={styles.widgetTitle}>
                                     Thông tin
                                 </h2>
-                                <div className="flex flex-wrap -mx-4">
-                                    <ul className="w-full">
+                                <div className="flex flex-wrap">
+                                    <ul className="w-full !list-none !p-0">
                                         <li>
                                             <Link href="/">Trang chủ</Link>
                                         </li>
@@ -91,16 +100,15 @@ export default function Footer() {
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full md:w-1/2 lg:w-1/4 px-4">
+                        <div className="w-full md:w-1/2 lg:w-1/4 px-3">
                             <div
                                 className={`${styles.footerBox} ${styles.pages}`}
                             >
                                 <h2 className={styles.widgetTitle}>
                                     Chính sách
                                 </h2>
-                                <div className="flex flex-wrap -mx-4">
-                                 
-                                    <ul className="w-full">
+                                <div className="flex flex-wrap">
+                                    <ul className="w-full !list-none !p-0">
                                         <li>
                                             <Link href="/news/2acb0ab0-9d43-42cb-9852-47abf821cb57">
                                                 Chính sách về thuế
@@ -111,7 +119,7 @@ export default function Footer() {
                                                 Chính sách mua hàng và đổi trả
                                             </Link>
                                         </li>
-                                        <li>    
+                                        <li>
                                             <Link href="/news/c6e3cfff-5c62-4d34-ac29-51764b6542d4">
                                                 Bảo mật thông tin
                                             </Link>
@@ -130,10 +138,13 @@ export default function Footer() {
             </div>
             <div className={`${styles.copyright}`}>
                 <div className="container">
+
                     <div className="grid grid-cols-1 md:grid-cols-2 items-center">
                         {/* Left */}
-                        <div>
-                            <p>
+                        <div className="py-4">
+                            <p className="m-0 !p-0">Giấy chứng nhận đăng ký doanh nghiệp: số 6300288132 do</p>
+                            <p className="m-0 !p-0">Sở kế hoạch và đầu tư tỉnh Hậu Giang cấp ngày 08 tháng 05 năm 2017</p>
+                            <p className="m-0 !p-0">
                                 Bản quyền © 2025 -{" "}
                                 <Link
                                     href="https://www.duoclieuxanhn22.com/"
@@ -155,7 +166,7 @@ export default function Footer() {
                         {/* Right */}
                         <div className="flex justify-start md:justify-end">
                             <ul
-                                className={`${styles.socialIcons} flex space-x-4`}
+                                className={`${styles.socialIcons} !list-none flex space-x-4`}
                             >
                                 <li>
                                     <Link href="/" target="_blank">
